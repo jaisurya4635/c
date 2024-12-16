1146eae4266ee8729ef89f700521825b286febc1
jaisurya4635 <jaisuryajaisurya623@gmail.com>
2024-12-16 15:32:20 +0530

    Create  Print numbers with decimal places.c

---
+++ Print numbers with decimal places.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a;
+    scanf("%f",&a);
+    printf("%f\n",a);
+    printf("%.3f\n",a);
+    printf("%.2f\n",a);
+    printf("%.1f\n",a);
+    return 0;
+}
