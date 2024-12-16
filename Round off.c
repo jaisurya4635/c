421b119c52fc62d9b0988419dc5128564e306f94
jaisurya4635 <jaisuryajaisurya623@gmail.com>
2024-12-16 15:35:23 +0530

    Create Round off.c

---
+++ Round off.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float a;
+    scanf("%f",&a);
+    printf("%d\n",(int)a);
+    printf("%.1f\n",ceil(a));
+     printf("%.1f",floor(a)); 
+    return 0;
+}
