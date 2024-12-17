5324f78976f01c2caade8d981560d528830924c0
jaisurya4635 <jaisuryajaisurya623@gmail.com>
2024-12-17 10:19:28 +0530

    Create Series - 6.c

---
+++ Series - 6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{ int a,b;
+scanf("%d",&a);
+for(int i=1;i<=a;i++)
+{    
+   b= pow(i,i);
+     printf("%d ",b);
+        
+    
+}
+
+
+
+	return 0;
+}
